bc1130cb089b843f409cfa5a7347d28ec0ded86b
pilar <pilar.rus.izquierdo@gmail.com>
2020-01-13 23:58:10 +0100

    Modified Connection

--- lib/Connection/Connection.php
+++ lib/Connection/Connection.php
@@ -7,7 +7,8 @@ class Connection {
 
     public static function getInstance(){
         if(self::$instance === null){
-            self::$instance = new Connector();
+            $string_json = file_get_contents("./config_app.json", FILE_USE_INCLUDE_PATH);
+            self::$instance = new Connector($string_json);
         }
         return self::$instance;
     }
--- lib/Connection/Connector.php
+++ lib/Connection/Connector.php
@@ -1,8 +1,21 @@
 <?php
+
 namespace lib\Connection;
+
 class Connector {
-    public function __construct()
-    {
-        echo "Me he creado";
+
+    protected $db;
+    protected $db_name;
+    protected $user;
+    protected $password;
+    protected $connector;
+
+    public function __construct($configFile) {
+        $config = json_decode($configFile, true);
+        //var_dump($config);
+        $this->db = $config['db'];
+        $this->db_name = $config['db_name'];
+        $this->user = $config['user'];
+        $this->password = $config['password'];
     }
 }
