3c601b1742fa1438d7bbf628fbfd8dd614ec4bcc
pilar <pilar.rus.izquierdo@gmail.com>
2020-01-14 22:59:01 +0100

    Added connect

--- lib/Connection/Connection.php
+++ lib/Connection/Connection.php
@@ -5,11 +5,15 @@ namespace lib\Connection;
 class Connection {
     protected static $instance = null;
 
-    public static function getInstance(){
-        if(self::$instance === null){
+    public static function getInstance() {
+        if (self::$instance === null) {
             $string_json = file_get_contents("./config_app.json", FILE_USE_INCLUDE_PATH);
             self::$instance = new Connector($string_json);
         }
         return self::$instance;
+    }
+    
+    public function __clone() {
+        
     }
 }
--- lib/Connection/Connector.php
+++ lib/Connection/Connector.php
@@ -1,21 +1,43 @@
 <?php
 
 namespace lib\Connection;
+use \PDO;
+use \PDOException;
 
 class Connector {
 
     protected $db;
-    protected $db_name;
+    protected $dsn;
     protected $user;
     protected $password;
     protected $connector;
 
     public function __construct($configFile) {
         $config = json_decode($configFile, true);
-        //var_dump($config);
+        var_dump($config);
         $this->db = $config['db'];
-        $this->db_name = $config['db_name'];
+        $this->dsn = $config['dsn'];
         $this->user = $config['user'];
         $this->password = $config['password'];
+        $this->connector = $this->connect();
+    }
+
+    public function connect() {
+        try {
+            $connector = new PDO($this->dsn, $this->user, $this->password,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+            $connector->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        } catch (PDOException $e) {
+            echo "<h2>No existe la base de datos, creándola</h2><br/>";
+            try {
+                $connector = new PDO($this->db, $this->user, $this->password);
+                $create = file_get_contents("./create.sql");
+                $statement = $connector->prepare($create);
+                $statement->execute();
+            } catch(PDOException $e) {
+                echo "<span>No se ha creado</span><br/>";
+                print "Error:" . $e->getMessage() . "<br/>";
+            }
+        }
+        return $connector;
     }
 }
